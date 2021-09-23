9ef64495a3680934d209bb894fedeb66ee19a178
AdrGia <boulanger.Adrien.75@gmail.com>
2021-09-23 12:51:35 +0200

    création du dossier middleware + sauce app js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const sauce = require('./models/sauce');
+
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://Piiquante:PimentdeCayenne33@cluster0.pnpp1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -22,6 +24,28 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
+app.post('/api/sauce', (req, res, next) => {
+	delete req.body._id;
+	const sauce = new sauce({
+		...req.body
+	});
+	sauve.save()
+		.then(() => res.status(201).json ({ message: 'Sauce enregistrée !'}))
+		.catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/sauce/:id', (req, res, next) =>{
+	sauce.findOne({_id: req.params.id})
+		.then(sauce => res.status(200).json(sauce))
+		.catch(error => res.status(404).json({ error}));
+});
+
+
+app.use('/api/sauce', (req, res, next) =>{
+	sauce.find()
+	.then(sauces => res.status(200).json(sauces))
+	.catch(error => res.status(400).json({ error}));
+});	
 app.use('/api/auth', userRoutes);
 
 
