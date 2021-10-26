d8826a47802786e892247dd883c2f8e2f532779a
AdrGia <boulanger.Adrien.75@gmail.com>
2021-10-26 15:22:14 +0200

    modifications

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,12 +5,13 @@ const router = express.Router();
 const sauceCtrl = require('../controllers/sauce');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer');
+const validate = require('../middleware/validate');
 
 router.get('/', auth, sauceCtrl.getAllSauce);
-router.get('/:id', auth, sauceCtrl.getOneSauce);
-router.post('/', auth, multer, sauceCtrl.createSauce);
-router.put('/:id', auth, multer, sauceCtrl.modifySauce);
-router.delete('/:id', auth, sauceCtrl.deleteSauce);
-router.post('/:id/like', auth, sauceCtrl.likeDislikeSauce);
+router.get('/:id', auth, validate.id, sauceCtrl.getOneSauce);
+router.post('/', auth, multer, validate.sauce, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, validate.id, validate.sauce, sauceCtrl.modifySauce);
+router.delete('/:id', auth, validate.id, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, validate.id, validate.like, sauceCtrl.likeDislikeSauce);
 
 module.exports = router;
